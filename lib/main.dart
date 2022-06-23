b747e0f7a86f83710fe31c8a2fb42cd1e07ac348
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-23 13:06:14 +0100

    Convert to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() => {runApp(const MyApp())};
+void main() => {runApp(MyApp())};
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // as good practice I declared the questionIndex property as var because it's intantiated with 0 then no need to declare as int
+  var questionIndex = 0;
+
+  var questions = [
+    "What\'s your favorite color ?",
+    "What\'s your favorite food ?",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Quizz'),
-        ),
-        body: const Text('My Body'),
-      ),
+          appBar: AppBar(
+            title: const Text('Quizz'),
+          ),
+          // we pass a children widget to the body its a generic type of Widget passed to the list to be specific
+          body: Column(
+            children: <Widget>[
+              Text(questions.elementAt(questionIndex)),
+              ElevatedButton(
+                child: Text('Answer 1'),
+                onPressed: (() => {
+                      questionIndex++,
+                      print('Answer 1 pressed'),
+                      print(questionIndex),
+                    }),
+              ),
+              ElevatedButton(
+                child: Text('Answer 2'),
+                onPressed: (() => print('Answer 2 pressed')),
+              ),
+              ElevatedButton(
+                child: Text('Answer 3'),
+                onPressed: (() => print('Answer 3 pressed')),
+              ),
+            ],
+          )),
     );
   }
 }
