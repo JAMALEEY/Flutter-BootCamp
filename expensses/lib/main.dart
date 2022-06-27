696d06a5d6fb31016ff3cfa0fb68aabf0347516c
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2022-06-27 10:49:54 +0100

    Add ListView

--- expensses/lib/main.dart
+++ expensses/lib/main.dart
@@ -25,21 +25,19 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [],
-                ),
+      body: Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [],
               ),
-              UserTransaction()
-            ],
-          ),
+            ),
+            UserTransaction()
+          ],
         ),
       ),
     );
--- expensses/lib/widgets/transaction_list.dart
+++ expensses/lib/widgets/transaction_list.dart
@@ -9,55 +9,58 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions
-          .map(
-            (tx) => Card(
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Colors.red,
-                      width: 1.5,
-                    )),
-                    child: Text(
-                      '${tx.amount}' + '\$',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.red),
+    return Container(
+      height: 300,
+      child: ListView(
+        children: transactions
+            .map(
+              (tx) => Card(
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.all(50),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.red,
+                        width: 1.5,
+                      )),
+                      child: Text(
+                        '${tx.amount}' + '\$',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.red),
+                      ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22,
-                          color: Colors.purple,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                            color: Colors.purple,
+                          ),
                         ),
-                      ),
-                      Text(
-                        DateFormat.yMMMd()
-                            .format(tx.transactionDate as DateTime),
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Color.fromARGB(255, 141, 141, 141),
+                        Text(
+                          DateFormat.yMMMd()
+                              .format(tx.transactionDate as DateTime),
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color.fromARGB(255, 141, 141, 141),
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                      ],
+                    )
+                  ],
+                ),
+                elevation: 5,
               ),
-              elevation: 5,
-            ),
-          )
-          .toList(),
+            )
+            .toList(),
+      ),
     );
   }
 }
